c6d84ab4073f0cdbfece0f987f24f0b0f0b089af
PedroPsousilva <pedro.soupsilva@gmail.com>
2025-02-05 13:50:39 -0300

    05-02

---
+++ atividade/src/Login/Login.jsx
@@ -0,0 +1,111 @@
+import * as React from "react";
+import  TextField  from "@mui/material/TextField"
+import Button from "@mui/material/Button"
+import Avatar from "@mui/material/Avatar"
+import Box from "@mui/material/Box"
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined"
+import Container from "@mui/material/Container"
+import Typography from "@mui/material/Typography"
+import {useState} from "react"
+
+function Login(){
+
+const [user, setUser] = useState({
+    email:"",
+    password:"",
+    age:"",
+    name:""
+
+})
+
+const onChange= (event) => {
+    const{ name , value} = event.target;
+    setUser({...user,[name]:value})
+
+};
+
+const handleSubmit = (event)  => {
+    event.preventDefault();
+    alert("Email:"+user.email+ " - " +"Senha:"+user.password+ " - Idade:"+user.age+" - " +"Nome:"+user.name)
+    
+
+}
+
+    return(
+        <Container component="main" maxWidth="xs">
+            <Box sx={{
+                marginTop:8,
+                display:"flex",
+                flexDirection:"column",
+                alignItems:"center"
+            }}>
+                <Avatar sx={{
+                    margin:1, backgroundColor:"#FF1800"
+                }}>
+                    <LockOutlinedIcon/>
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Vio
+                </Typography>
+                <Box component="form" sx={{mt:1}} onSubmit={handleSubmit}>
+                    <TextField
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email"
+                    name="email"
+                    margin="normal"
+                    value={user.email}
+                    onChange={onChange}
+                    
+                    />
+                    <TextField
+                    required
+                    fullWidth
+                    id="password"
+                    label="Senha"
+                    name="password"
+                    margin="normal"
+                    type="password"
+                    value={user.password}
+                    onChange={onChange}
+                    />
+                     <TextField
+                    required
+                    fullWidth
+                    id="age"
+                    label="Idade"
+                    name="age"
+                    margin="normal"
+                    type="number"
+                    value={user.age}
+                    onChange={onChange}
+                    />
+                     <TextField
+                    required
+                    fullWidth
+                    id="name"
+                    label="Nome"
+                    name="name"
+                    margin="normal"
+                    value={user.name}
+                    
+                    onChange={onChange}
+                    />
+
+                    
+                    <Button sx={{mt:3 , mb:2, backgroundColor:"#FF1800"}}
+                    fullWidth
+                    type="submit"
+                    variant="contained"
+                    >Entrar</Button>
+
+                </Box>
+            </Box>
+
+        </Container>
+    )
+
+}
+
+export default Login;
